a23e1d5a40fd9b495b919ee3b0858785d802574e
MoldirKopbay <55589241+MoldirKopbay@users.noreply.github.com>
2019-09-23 17:55:45 +0600

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<style>
-		body {
-			background-color:  #5F9EA0;
-			padding-top: 50px;
-		}
-	</style>
+<style>
+	body { background-color:  #5F9EA0;
+		padding-top: 50px;
+	}
+</style>
 </head>
 <body>
 	<h1 style="text-align: center; color: white">WELCOME !!!</h1>
